05c66e362b6a9681f814408fb76fe33211309889
mohittk <mvk1407@gmail.com>
2022-02-14 10:07:22 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,60 @@
-import React from 'react';
-import {Grid} from '@material-ui/core';
-import Details from './components/details/details';
-import useStyles from './styles';
-import Main from './components/main/main';
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Details from "./components/details/details";
+import useStyles from "./styles";
+import Main from "./components/main/main";
+import firebase from "firebase/compat/app";
 // import ('https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Jockey+One&family=Merriweather+Sans:wght@500&family=Poppins:wght@300&family=Ruslan+Display&display=swap');
 
-const App = () =>{
-    const classes = useStyles();
-    return(
-        <>
-        <div className="title">
-            <p style={{color: 'white', fontSize: '5rem', textAlign: 'center', fontFamily: 'Azeret Mono'}}>E<span style={{color: 'yellow'}}>x</span>p<span style={{color: 'cyan'}}>e</span>n$<span style={{color: '	#FF1493'}}>o</span></p>
-        </div>
-        <div>
-        <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{height : '60vh'}} >
-            <Grid item xs={12} sm={4}>
-                <Details title="Income" />
-            </Grid>
-            <Grid item xs={12} sm={3}>
-                <Main />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-                <Details title="Expense" />
-            </Grid>
-            </Grid>
+const App = () => {
+  const classes = useStyles();
+  const firebaseApp = firebase.apps[0];
+  return (
+    <>
+      <div>
+        <h1>React & Firebase</h1>
+        <h2>By @farazamiruddin</h2>
+        <code>
+          <p>{JSON.stringify(firebaseApp.options, null, 2)}</p>
+        </code>
+      </div>
 
-        </div>
-        </>
-    )
-}
+      <div className="title">
+        <p
+          style={{
+            color: "white",
+            fontSize: "5rem",
+            textAlign: "center",
+            fontFamily: "Azeret Mono",
+          }}
+        >
+          E<span style={{ color: "yellow" }}>x</span>p
+          <span style={{ color: "cyan" }}>e</span>n$
+          <span style={{ color: "	#FF1493" }}>o</span>
+        </p>
+      </div>
+      <div>
+        <Grid
+          className={classes.grid}
+          container
+          spacing={0}
+          alignItems="center"
+          justify="center"
+          style={{ height: "60vh" }}
+        >
+          <Grid item xs={12} sm={4}>
+            <Details title="Income" />
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <Main />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Details title="Expense" />
+          </Grid>
+        </Grid>
+      </div>
+    </>
+  );
+};
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from "react-dom";
 import App from './App';
 import { Provider } from "./context/context";
 import './index.css';
+import firebase from "firebase/compat/app";
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDcg_3FogwyFNNpc66LCc56tfqkhmY-8gw",
+    authDomain: "expenso-a33e5.firebaseapp.com",
+    projectId: "expenso-a33e5",
+    storageBucket: "expenso-a33e5.appspot.com",
+    messagingSenderId: "291595298909",
+    appId: "1:291595298909:web:4fc217652e5134c11d45b4"
+});
+  
+
 
 ReactDOM.render(
     <Provider>
